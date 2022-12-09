3ac721fd0c3a9ae212c563bda2ab4bfbdb387fd8
NandoLopes <54965130+NandoLopes@users.noreply.github.com>
2022-12-09 18:59:14 -0300

    Feat: Descrição dos serviços

--- src/app/components/servicos/cadastro-servico/cadastro-servico.component.ts
+++ src/app/components/servicos/cadastro-servico/cadastro-servico.component.ts
@@ -1,4 +1,3 @@
-import { AnimationKeyframesSequenceMetadata } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -26,17 +25,29 @@ export class CadastroServicoComponent implements OnInit {
   ngOnInit() {
     this.validacao();
     
+    //Tenta pegar o id passado pela rota, e se existir e for maior que 0, busca o servico pela service
     this.routeId =  this.activatedRoute.snapshot.paramMap.get("id");
     if(this.routeId && this.routeId > 0){
       this.carregarServico(this.routeId);
     }
   }
 
+  //Limpa todos os campos menos o Id (pra evitar criar um novo serviço após limpar campos de um ja existente)
+  limparForm(){
+    this.registerForm.controls['Nome'].reset();
+    this.registerForm.controls['Duracao'].reset();
+    this.registerForm.controls['Valor'].reset();
+  }
+
   cadastrar() {
+    //Se o form estiver com dados válidos, prossegue
     if(this.registerForm.valid){
+      //Preenche o servico com os dados do form
       this.servico = {...this.servico, ...this.registerForm.value};
+      //Se o serviço tiver um id, significa que é um edit, se não tiver, é um create
       this.servico.Id ? this.editarServico(this.servico) : this.criarServico(this.servico);
 
+      //Manda de volta pra tela de listagem dos serviços
       this.router.navigate(
         ['/servicos']
       );
@@ -51,11 +62,13 @@ export class CadastroServicoComponent implements OnInit {
     console.log(this.servicoService.putServico(servico));
   }
 
+  //Busca serviço na service pelo Id, e então preenche o form com os dados dele
   carregarServico(id: number){
     this.servico = this.servicoService.getServicoById(id);
     this.registerForm.patchValue(this.servico);
   }
 
+  //Cria o form
   validacao(){
     this.registerForm = this.fb.group({
       Id: [],
